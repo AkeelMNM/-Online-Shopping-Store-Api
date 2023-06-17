9ffa59af3912831c4f8bf147f618ebac6577a02a
Mohamed Akeel <akeel1447@gmail.com>
2023-06-17 21:19:54 +0530

    changing server from https to http

--- authenticate.ts
+++ authenticate.ts
@@ -1,22 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
-import https from 'https';
+// import https from 'https';
+import http from 'http';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const rootCas = require('ssl-root-cas').create();
-rootCas.addFile(__dirname + process.env.SSL_ROOT_CA_CERTIFICATE_PATH || '');
-https.globalAgent.options.ca = rootCas;
+// const rootCas = require('ssl-root-cas').create();
+// rootCas.addFile(__dirname + process.env.SSL_ROOT_CA_CERTIFICATE_PATH || '');
+// https.globalAgent.options.ca = rootCas;
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
     const queryParams = {
-        host: 'localhost',
-        port: 8443,
+        host: process.env.AUTHENTICATION_HOST,
+        port: process.env.AUTHENTICATION_PORT,
         path: '/user/authenticate',
         method: 'POST',
         headers: { 'Content-type': 'application/json', 'Cookie': req.cookies.jwtToken },
     };
 
-    const sentRequest = https.request(queryParams, (res) => {
+    const sentRequest = http.request(queryParams, (res) => {
         res.setEncoding('utf8');
         res.on('data', (body) => {
             let responseData = JSON.parse(body);
